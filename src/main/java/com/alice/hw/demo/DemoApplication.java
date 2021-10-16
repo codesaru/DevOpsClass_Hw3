8b0fcfc16adfe15eb5e3ba387db4d982b0d7cab0
student <alice.wang@ngc.com>
2021-10-16 23:51:21 +0000

    test change

--- src/main/java/com/alice/hw/demo/DemoApplication.java
+++ src/main/java/com/alice/hw/demo/DemoApplication.java
@@ -28,6 +28,6 @@ public class DemoApplication extends SpringBootServletInitializer {
   public String hello(
     @RequestParam(value = "name", defaultValue = "World") String name
   ) {
-    return String.format("Hello %s!", name);
+    return String.format("Nihao %s!", name);
   }
 }
