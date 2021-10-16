388200b74b84b86bc2d9fa2fdb38c9699a57e1c4
student <alice.wang@ngc.com>
2021-10-16 23:28:57 +0000

    init

---
+++ src/main/java/com/alice/hw/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.alice.hw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) 
+  {
+    return builder.sources(DemoApplication.class);
+  }
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @GetMapping
+  public String hello(
+    @RequestParam(value = "name", defaultValue = "World") String name
+  ) {
+    return String.format("Hello %s!", name);
+  }
+}
