dbbe0043f017cf7a9337f37707b3b2b7340cd748
Ubuntu <ubuntu@ip-172-31-25-48.ec2.internal>
2021-10-30 00:47:23 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mvn package
-sudo docker build -t capstone codesarukiller/capstone .
+sudo docker stop capstone
+sudo docker build -t codesarukiller/capstone .
 sudo docker run -d --name capstone --rm -p 8888:8080 codesarukiller/capstone
 sudo docker push codesarukiller/capstone:latest
